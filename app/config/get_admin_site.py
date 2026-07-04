f2783035a952c3b249bf95628ad2b9c1a848440e
Mr. EnglishCat <mr.englishcat@gmail.com>
2026-07-04 23:53:59 +0300

    refactoring CRUDAdmin

---
+++ app/config/get_admin_site.py
@@ -0,0 +1,62 @@
+from crudadmin import CRUDAdmin, RedisConfig
+
+from app.config.db import get_session
+from app.models.evaluation import EvaluationModel
+from app.models.meetings import MeetingModel
+from app.models.tasks import TaskModel
+from app.models.teams import TeamModel
+from app.models.users import UserModel
+from app.schemas.evaluation import EvaluationBaseSchema, EvaluationResponseSchema
+from app.schemas.meetings import MeetingBaseSchema, MeetingResponseSchema
+from app.schemas.tasks import TaskBaseSchema, TaskResponseSchema
+from app.schemas.teams import TeamBaseSchema, TeamResponseSchema
+from app.schemas.users import UserBaseSchema, UserCreateSchema, UserResponseSchema
+
+redis_config = RedisConfig(host="localhost", port=6379, db=0)
+admin = CRUDAdmin(
+    session=get_session,
+    SECRET_KEY="your-secret-key",
+    session_backend="redis",
+    redis_config=redis_config,
+    initial_admin={
+        "username": "admin",
+        "password": "admin",  # В продакшене задать надежный пароль!
+    },
+)
+admin.add_view(
+    model=UserModel,
+    create_schema=UserCreateSchema,
+    update_schema=UserBaseSchema,
+    select_schema=UserResponseSchema,
+    allowed_actions={"view", "create", "update", "delete"},
+    display_field="username",
+)
+admin.add_view(
+    model=TeamModel,
+    create_schema=TeamBaseSchema,
+    update_schema=TeamBaseSchema,
+    select_schema=TeamResponseSchema,
+    allowed_actions={"view", "create", "update", "delete"},
+    display_field="name",
+)
+admin.add_view(
+    model=TaskModel,
+    create_schema=TaskBaseSchema,
+    update_schema=TaskBaseSchema,
+    select_schema=TaskResponseSchema,
+    display_field="title",
+)
+admin.add_view(
+    model=MeetingModel,
+    create_schema=MeetingBaseSchema,
+    update_schema=MeetingBaseSchema,
+    select_schema=MeetingResponseSchema,
+    display_field="title",
+)
+admin.add_view(
+    model=EvaluationModel,
+    create_schema=EvaluationBaseSchema,
+    update_schema=EvaluationBaseSchema,
+    select_schema=EvaluationResponseSchema,
+    display_field="score",
+)
--- main.py
+++ main.py
@@ -1,5 +1,34 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.v1_router import v1_router
 
-bms_app = FastAPI(title="Business Management System")
+# Настройка Redis
+from contextlib import asynccontextmanager
+from app.config.get_admin_site import admin
+
+
+@asynccontextmanager
+async def lifespan_app(app: FastAPI):
+    await admin.initialize()
+    yield
+
+
+bms_app = FastAPI(title="Business Management System", lifespan=lifespan_app, debug=True)
+
+
+bms_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+bms_app.mount("/admin", admin.app, "Admin")
 bms_app.include_router(v1_router)
+
+"""Управление договорами - Типы договоров - Редактирование типа. 
+
+Сделать не кликабельными коды типов договоров. Так де как и некликабельно название договоров. 
+"""
