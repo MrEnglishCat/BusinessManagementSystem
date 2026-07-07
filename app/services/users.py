e2a3fe112f978a8fd0993a2cfab7445cafb54141
Mr. EnglishCat <mr.englishcat@gmail.com>
2026-07-08 01:49:22 +0300

    remove delete method

--- app/services/users.py
+++ app/services/users.py
@@ -19,9 +19,3 @@ class UserService(BaseService):
         if user:
             return UserResponseSchema.model_validate(user)
         return None
-
-    async def delete(self, session, **filter_by):
-        user = await super().delete(session, **filter_by)
-        if user:
-            return user
-        return None
