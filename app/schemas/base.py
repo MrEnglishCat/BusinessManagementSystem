c54aa6605be8210a05676cf968bb21ed47c9246f
Mr. EnglishCat <mr.englishcat@gmail.com>
2026-07-09 22:02:20 +0300

    BMS - 4
    - add BaseDataTimePydanticModel

--- app/schemas/base.py
+++ app/schemas/base.py
@@ -1,5 +1,15 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
+from datetime import datetime
 
 
 class BasePydanticModel(BaseModel):
     model_config = ConfigDict(from_attributes=True)
+
+
+class BaseDataTimePydanticModel(BasePydanticModel):
+    created_at: datetime = Field(
+        title="Created at", json_schema_extra={"example": "2026-05-29"}
+    )
+    updated_at: datetime = Field(
+        title="Updated at", json_schema_extra={"example": "2026-05-29 23:23"}
+    )
